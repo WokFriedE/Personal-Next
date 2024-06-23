f8b030fe0612fb2fe4634ab9d442afa29bccb00f
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-06-23 19:30:13 -0400

    Small edits

---
+++ src/app/components/item_display.js
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function DisplayContainer(props) {
+
+    const [innerData, setInnerData] = useState("");
+
+    return (
+        <div className="border-gray-500 border flex-1 relative m-2 min-w-52 items-center justify-center flex rounded-md">
+            <p className="absolute -top-3.5 left-2 bg-gray-900 px-2 rounded-lg border-gray-500 border ">{props.title}</p>
+            <div className="flex px-3 py-4 flex-wrap items-center justify-center">
+                {props.item.map((item) => (
+                    <p className="flex-initial px-4 rounded-full border-2 border-neutral-700 m-1 text-center" style={{ hover: {} }}>
+                        {item.name}
+                    </p>
+                )
+                )}
+            </div>
+        </div>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,23 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import languagesJSON from "../static/data/languages.json";
+import skillsJSON from "../static/data/skills.json";
+import toolsJSON from "../static/data/tools.json";
+
+import ItemContainer from "./components/item_display";
 
 export default function Home() {
+    const [langs, setLangs] = useState([]);
+    const [tools, setTools] = useState([]);
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+        setLangs(languagesJSON);
+        setSkills(skillsJSON);
+        setTools(toolsJSON);
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col py-10">
             <p className="text-4xl text-center">
@@ -9,15 +26,14 @@ export default function Home() {
             <p className="text-xl text-center">
                 Computer Science, B.S.
             </p>
-            <div className="flex flex-wrap py-10 space-x-5 px-10">
+            <div className="flex flex-wrap pt-8 px-10">
                 {/*   <button className="bg-gray-300 text-gray-700 flex-2 px-6 py-1 text-lg rounded-full">Tech Portfolio</button>
                 <button className="bg-gray-300 text-gray-700 flex-2 px-6 py-1 text-lg rounded-full">Photo Portfolio</button> */}
-                <div className="border-gray-500 border grow relative">
-                    <p className="absolute -top-3.5 left-2 bg-gray-900 px-2 rounded-lg border-gray-500 border ">Tools</p>
-                </div>
-                <div className="border-gray-500 border grow relative">
-                    <p className="absolute -top-3.5 left-2 bg-gray-900 px-2 rounded-lg border-gray-500 border ">Skill</p>
-                </div>
+                <ItemContainer title="Tools" item={tools} />
+                <ItemContainer title="Skills" item={skills} />
+            </div>
+            <div className="flex flex-wrap py-1 px-10">
+                <ItemContainer title="Languages" item={langs} />
             </div>
         </main>
     );
