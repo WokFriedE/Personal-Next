1b27d54a17cc4570617d365c9d4ec6602934f678
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-06-23 20:47:09 -0400

    Added Icons

--- src/app/components/item_display.js
+++ src/app/components/item_display.js
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import { Icon } from "@iconify/react";
 
 export default function DisplayContainer(props) {
 
@@ -10,13 +11,16 @@ export default function DisplayContainer(props) {
         <div className="border-gray-500 border flex-1 relative m-2 min-w-52 items-center justify-center flex rounded-md">
             <p className="absolute -top-3.5 left-2 bg-gray-900 px-2 rounded-lg border-gray-500 border ">{props.title}</p>
             <div className="flex px-3 py-4 flex-wrap items-center justify-center">
-                {props.item.map((item) => (
-                    <p className="flex-initial px-4 rounded-full border-2 border-neutral-700 m-1 text-center" style={{ hover: {} }}>
+                {props.item.map((item) => (<div className="flex-initial px-4 rounded-full border-2 border-neutral-700 m-1 flex items-center gap-x-1">
+                    {item.icon &&
+                        <Icon icon={item.icon}></Icon>}
+                    <p p className="text-center" style={{ hover: {} }}>
                         {item.name}
                     </p>
+                </div>
                 )
                 )}
             </div>
-        </div>
+        </div >
     );
 }
