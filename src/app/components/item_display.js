05bc55c8a5260316b91b389b99a2e5cf7fe445ec
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-06-24 00:56:30 -0400

    Final update to skills

--- src/app/components/item_display.js
+++ src/app/components/item_display.js
@@ -5,16 +5,16 @@ import { Icon } from "@iconify/react";
 
 export default function DisplayContainer(props) {
 
-    const [innerData, setInnerData] = useState("");
-
     return (
-        <div className="border-gray-500 border flex-1 relative m-2 min-w-52 items-center justify-center flex rounded-md">
-            <p className="absolute -top-3.5 left-2 bg-gray-900 px-2 rounded-lg border-gray-500 border ">{props.title}</p>
-            <div className="flex px-3 py-4 flex-wrap items-center justify-center">
-                {props.item.map((item) => (<div className="flex-initial px-4 rounded-full border-2 border-neutral-700 m-1 flex items-center gap-x-1">
+        <div className="p-5 flex flex-col items-center">
+            {props.icon &&
+                <Icon icon={props.icon} className="text-4xl my-1" />}
+            <h4 className="md:text-2xl sm:text-xl text-sm pb-2 text-center capitalize ">{props.title}</h4>
+            <div className="flex flex-wrap items-center justify-center">
+                {props.item.map((item) => (<div className="flex flex-initial sm:my-2 px-4 rounded-full border-2 border-neutral-700 m-1 items-center gap-x-1">
                     {item.icon &&
-                        <Icon icon={item.icon}></Icon>}
-                    <p p className="text-center" style={{ hover: {} }}>
+                        <Icon icon={item.icon} />}
+                    <p p className="text-center">
                         {item.name}
                     </p>
                 </div>
--- src/app/page.js
+++ src/app/page.js
@@ -8,43 +8,28 @@ import ItemContainer from "./components/item_display";
 // Change the icons to b/w
 
 export default function Home() {
-    // const [langs, setLangs] = useState([]);
-    // const [tools, setTools] = useState([]);
-    // const [skills, setSkills] = useState([]);
-
-    // useEffect(() => {
-    //     setLangs(languagesJSON);
-    //     setSkills(skillsJSON);
-    //     setTools(toolsJSON);
-    // }, []);
 
     return (
         <main className="flex min-h-screen flex-col py-10">
             <div>
-                <p className="text-4xl text-center">
+                <p className="text-5xl text-center">
                     Ethan Ho
                 </p>
-                <p className="text-xl text-center">
+                <p className="text-2xl text-center">
                     Computer Science, B.S.
                 </p>
             </div>
-            <h1 className="text-2xl mt-5 py-1 px-auto mx-20 2xl:mx-32">SKILLS</h1>
-            <div className="bg-slate-600/[.2] rounded-md p-1 2xl:mx-32 mx-12 border-2 border-gray-500 py-2">
-                <div className="flex flex-wrap pt-2 px-10">
-                    {/*   <button className="bg-gray-300 text-gray-700 flex-2 px-6 py-1 text-lg rounded-full">Tech Portfolio</button>
-<button className="bg-gray-300 text-gray-700 flex-2 px-6 py-1 text-lg rounded-full">Photo Portfolio</button> */}
-                    <ItemContainer title="Tools" item={toolsJSON} />
-                    <ItemContainer title="Soft Skills" item={skillsJSON} />
-                </div>
-                <div className="flex flex-wrap py-1 px-10">
-                    <ItemContainer title="Languages" item={languagesJSON} />
+            <h1 className="2xl:mx-32 sm:mx-12 mx-1 text-3xl mt-5 py-1 px-auto font-bold uppercase">Skills</h1>
+            <div className="2xl:mx-32 sm:mx-12 mx-1 bg-slate-600/[.2] rounded-lg border-2 border-gray-500">
+                <div className=" grid md:grid-cols-3 md:divide-x sm:grid-cols-1 divide-y md:divide-y-0 divide-solid">
+                    <ItemContainer item={languagesJSON} title="Languages" icon="heroicons-outline:code" />
+                    <ItemContainer item={skillsJSON} title="Skills" icon="heroicons-outline:user" />
+                    <ItemContainer item={toolsJSON} title="Tools" icon="heroicons-outline:computer-desktop" />
                 </div>
             </div>
-            <h1 className="text-2xl mt-5 py-1 px-auto mx-20 2xl:mx-32">EXTRACURRICULARS</h1>
-            <div className="bg-slate-600/[.2] rounded-md p-1 2xl:mx-32 mx-12 border-2 border-gray-500 py-2">
+            <h1 className="2xl:mx-32 sm:mx-12 mx-1 text-3xl mt-5 py-1 px-auto font-bold uppercase">Extracurricular</h1>
+            <div className="2xl:mx-32 sm:mx-12 mx-1 bg-slate-600/[.2] rounded-lg border-2 border-gray-500">
             </div>
         </main>
     );
 }
-
-
