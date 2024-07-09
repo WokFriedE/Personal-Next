2918fc2a2cb88360dd557b3398825b8db88fa3ea
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-07-09 19:23:21 -0400

    added extracurricular sorting

--- src/app/page.js
+++ src/app/page.js
@@ -7,34 +7,14 @@ import toolsJSON from "../static/data/tools.json";
 import extracurricularJSON from "../static/data/manual/extracurricular.json";
 import ItemContainer from "./components/item_display";
 import Debugger from "./components/debug";
+import sortExtracurricular from "../../lib/sortExtracurricular";
 
 // Change the icons to b/w
 
 export default function Home() {
     // Handle Extracurricular data
-    let extraData = [...extracurricularJSON];
-    const sortPos = (aPos, bPos) => {
-        // If both current
-        if (aPos.current && bPos.current && aPos.start > bPos.start) return 1;
-        else if (bPos.current && aPos.current && bPos.start > aPos.start) return -1;
-        // If one is current and one isn't
-        else if (aPos.current && !bPos.current) return -1;
-        else if (!aPos.current && bPos.current) return 1;
-        // test end times (past)
-        else if (aPos.end > bPos.end) return 1;
-        else if (aPos.end < bPos.end) return -1;
-        else return 0;
-    };
-    // Sort positions for each org
-    extraData.forEach((item, i) => {
-        extraData[i].positions = item.positions.sort((a, b) => {
-            return sortPos(a, b);
-        });
-    });
-    // Sort each org so present is top
-    extraData.sort((a, b) => {
-        return sortPos(a.positions[0], b.positions[0]);
-    });
+    let extraData = sortExtracurricular(extracurricularJSON);
+    console.log(extraData);
 
     return (
         <main className="flex min-h-screen flex-col py-10">
