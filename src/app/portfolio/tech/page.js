1d9f202159b0546763ef3048564fae8d52486cd1
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-07-11 01:20:52 -0400

    Projects page init

---
+++ src/app/portfolio/tech/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TechPage() {
+    return (
+        <div className="flex min-h-max flex-col">
+            <p>hello</p>
+        </div>
+    );
+}
+
+export default TechPage;
